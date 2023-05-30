55fd353ff67819a1c72c94e5e428aae9954d2aad
YerkenBek <yerkeny@gmail.com>
2023-05-30 21:18:11 +0600

    1

---
+++ 30.05/30.05.03.py
@@ -0,0 +1,10 @@
+cost = 1500
+if cost < 1000:
+    print("скидок нет.")
+if cost < 2000:
+    print("скидка 2%.")
+if cost < 5000:
+    print("скидка 5%.")
+else:
+    print("скидка 10%.")
+print("конец")
