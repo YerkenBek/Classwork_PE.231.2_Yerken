9ba9d2035e0d8fcc41ff0e57e84dea775f5dd7ab
YerkenBek <yerkeny@gmail.com>
2023-06-02 20:23:19 +0600

    Update 02.06.01.py

--- 02.06/02.06.01.py
+++ 02.06/02.06.01.py
@@ -1,11 +1,11 @@
-def show_manty_ing(total_kg, portions=99):
+def show_manty_ing(total_kg=1):
     meat    = 0.5 * total_kg
     flour   = 0.2 * total_kg
     carrot  = 0.05 * total_kg
     pumpkin = 0.1 * total_kg
     onion   = 0.1 * total_kg
     potato  = 0.1 * total_kg
-    template = """To prepare %s kg, for %s portions manty you will need:
+    template = """To prepare %s kg manty you will need:
     - мясо: %.2f кг,
     - мука: %.2f кг,
     - морковь: %.2f кг,
@@ -13,12 +13,13 @@ def show_manty_ing(total_kg, portions=99):
     - лук: %.2f кг,
     - картофель: %.2f кг.
     """
-    message = template % (total_kg, portions, meat, flour, carrot, pumpkin, onion, potato)
+    message = template % (total_kg, meat, flour, carrot, pumpkin, onion, potato)
     print(message)
+    return int(total_kg * 4) # number of portions
 
 user_in = input("How many kg of mantys do you need? ")
 user_kg = float(user_in)
 
-show_manty_ing(user_kg, 6)
-
+portions = show_manty_ing(user_kg)
+print("Total portions done:", portions)
 
