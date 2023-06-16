3e173ffe27ab2513a180c42bb3d37aedaa738eec
YerkenBek <yerkeny@gmail.com>
2023-06-16 21:25:55 +0600

    1

--- 16.06/16.06.02.py
+++ 16.06/16.06.02.py
@@ -62,6 +62,31 @@ class Kassa:
         else:
             print("no money - no ticket!")
     
+class Train:
+    source = ""
+    destination = ""
+    number = 0
+
+    def __init__(self, source, destination):
+        self.source = source
+        self.destination = destination
+        self.number = randint(100, 999)
+
+    def board(self, person):
+        if person.ticket:
+            if self.source == person.ticket.source and self.destination == person.ticket.destination:
+                msg = "Welcome on board %s, Your train #%s arrived, from %s to %s." % (person.name, self.number,
+                                                                                        self.source, self.destination)
+                print(msg)
+                person.ticket = None
+            else:
+                print("Wrong ticket number")
+        else:
+            print("No tickets")
+
+    def show(self):
+        msg = "Train #%s, from station %s to station %s." % (self.number, self.source, self.destination)
+        print(msg)
 
 
 test_man = Person("Ilon Mask", 56)
@@ -79,4 +104,9 @@ kassa.buy_ticket("Almaty", "Santiago", test_man)
 if test_man.ticket:
     test_man.ticket.show()
 
-print(price)
+train = Train("Almaty", "Santiago")
+train.show()
+
+train.board(test_man)
+if test_man.ticket is None:
+    print("Ticket is over")
